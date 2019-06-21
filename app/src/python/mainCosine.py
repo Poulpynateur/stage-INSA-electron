0790e3d20fe14547669b6fcb733cb85a188bca6f
Poulpynateur <45163494+Poulpynateur@users.noreply.github.com>
2019-06-21 17:00:34 +0200

    Changing npm (?) to nlp

--- app/src/python/mainCosine.py
+++ app/src/python/mainCosine.py
@@ -1,19 +1,19 @@
 # Main file to calculate cosine-similarity
 
-import src.npm as npm
+import src.nlp as nlp
 from sklearn.metrics.pairwise import cosine_similarity
 
-test, training = npm.load_json(
+test, training = nlp.load_json(
     'ressources/train/training_set.json', 'iso-8859-1',
     'ressources/scraped/archive/jeb.biologists.org.2019.articles.json', 'utf-8'
 )
 
-train_data, test_data = npm.clean(test, training, 'title_annotation', 'title')
+train_data, test_data = nlp.clean(test, training, 'title_annotation', 'title')
 
 #Passing only the selected fields to vectorizerTFIDF()
-train_tfidf, test_tfidf = npm.vectorizerTFIDF(train_data[:,0], test_data )
+train_tfidf, test_tfidf = nlp.vectorizerTFIDF(train_data[:,0], test_data )
 
 cosine_results = cosine_similarity(test_tfidf, train_tfidf)
 
 index = 40;
-npm.show(cosine_results[index] , train_data[:,1])
+nlp.show(cosine_results[index] , train_data[:,1])
--- app/src/python/mainModels.py
+++ app/src/python/mainModels.py
@@ -1,14 +1,14 @@
 # Main file to use models from src.models
 
-import src.npm as npm
+import src.nlp as nlp
 import src.models as models
 
-test, training = npm.load_json(
+test, training = nlp.load_json(
     'ressources/train/training_set.json', 'iso-8859-1',
     'ressources/scraped/archive/jeb.biologists.org.2019.articles.json', 'utf-8'
 )
 
-train_data, test_data = npm.clean(test, training, 'abstract_annotation', 'abstract')
+train_data, test_data = nlp.clean(test, training, 'abstract_annotation', 'abstract')
 
 abstracts = train_data[:,0]
 principles = train_data[:,1]
